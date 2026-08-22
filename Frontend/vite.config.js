8f84a0fedb742d43d30753f8f5d3d60470441224
priyanshu das <das2004sonu@gmail.com>
2026-08-22 19:45:49 +0530

    chore: remove all duplicate files across frontend public and assets

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      '@/assets': path.resolve(__dirname, '../assets'),
       '@': path.resolve(__dirname, './src'),
     },
   },
